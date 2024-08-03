45b737b3126edb65646e1d483c4c0742e2ebbeee
Nihaluday <nihaludayraikar@gmail.com>
2024-08-03 10:52:02 +0530

    Add files via upload

---
+++ functionprops.js
@@ -0,0 +1,13 @@
+import React from "react";
+function Functionprops(props){
+    return(
+        <div>
+        <h3> this is functional component</h3>
+        <h3>
+        Hello{props.name} from{props.place}Welcome toreact.js props
+        </h3>
+        </div>
+    );
+    }
+    export default Functionprops;
+
